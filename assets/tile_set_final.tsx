e82790618982c7dcde49de624bed569f353c49c0
MioYss <arnaudhung@gmail.com>
2023-01-24 17:13:25 +0100

    Toujours en depression mais j'aurai pas 0

---
+++ assets/tile_set_final.tsx
@@ -0,0 +1,226 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tile_set_final" tilewidth="32" tileheight="32" tilecount="3900" columns="78">
+ <image source="tuiles_jeu.png" width="2500" height="1600"/>
+ <tile id="0">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="156">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="158">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="160">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="162">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="312">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="314">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="316">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="318">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="320">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="322">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="324">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="326">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="328">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="330">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="468">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="470">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="472">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="474">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="476">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="478">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="480">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="482">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="624">
+  <properties>
+   <property name="estCorde" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="626">
+  <properties>
+   <property name="estCorde" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="628">
+  <properties>
+   <property name="estCorde" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="630">
+  <properties>
+   <property name="estCorde" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="780">
+  <properties>
+   <property name="estPiege" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="782">
+  <properties>
+   <property name="estPiege" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="936">
+  <properties>
+   <property name="estPlatMouv" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="938">
+  <properties>
+   <property name="estPlatMouv" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1092">
+  <properties>
+   <property name="estPiege" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1094">
+  <properties>
+   <property name="estPiege" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1096">
+  <properties>
+   <property name="estPiege" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1098">
+  <properties>
+   <property name="estPiege" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1100">
+  <properties>
+   <property name="estPiege" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1102">
+  <properties>
+   <property name="estPiege" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1404">
+  <properties>
+   <property name="estCoeur" type="bool" value="true"/>
+   <property name="estCoffre" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="1406">
+  <properties>
+   <property name="estCoeur" type="bool" value="true"/>
+   <property name="estCoffre" type="bool" value="false"/>
+  </properties>
+ </tile>
+</tileset>
